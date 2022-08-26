8ab044dd073f74487cd2e50e7357091589d61ad1
vish-saxena <94616788+vish-saxena@users.noreply.github.com>
2022-08-26 15:26:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,42 +8,42 @@ const App = function () {
     image : 'https://www.deccanherald.com/sites/dh/files/article_images/2020/05/19/World-470707478-1581845039.jpg',
     tittle : 'World Famous Lover',
     aurthor : 'Vijay Deverakonda,',
-    price : '₹499',
+    price : 'Rating: 9.8/10 ',
   };
 
   const secondMovie = {
     image : 'https://wallpaperaccess.com/full/3566179.jpg',
     tittle : 'Taxiwala',
     aurthor : 'Vijay Deverakonda',
-    price : '₹ 159',
+    price : 'Rating: 8.7/10 ', 
   };
 
   const thirdMovie = {
     image : 'https://m.media-amazon.com/images/M/MV5BOTdhODUxYzYtNzc1Zi00YTMzLThhYzAtMTk3YzIwMjQwYzMwXkEyXkFqcGdeQXVyNTgxODY5ODI@._V1_.jpg',
     tittle : 'Bheeshma',
     aurthor : 'Nithiin, Rashmika',
-    price : '₹249',
+    price : 'Rating: 8.4/10 ',
   }
 
   const fourthMovie = {
     image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJpY7nqTEJqjOkzMsYfSEN6Y_KWsM1q1I6Xw&usqp=CAU',
     tittle : 'Dear Comrade',
-    aurthor : 'Vijay',
-    price : '₹400',
+    aurthor : 'Vijay, Rashmika',
+    price : 'Rating: 9/10 ',
   }
 
   const fifthMovie = {
     image : 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Tadap_film_poster.jpg/220px-Tadap_film_poster.jpg',
     tittle : 'Tadap',
     aurthor : 'Ahan Shetty',
-    price : '149',
+    price : 'Rating: 6.9/10 ',
   }
 
   const sixthMovie = {
     image : 'https://m.media-amazon.com/images/M/MV5BZWE0MGM3YzEtM2ZmNi00NmMyLWE5OGEtMGJkNjVlOThhOTEwXkEyXkFqcGdeQXVyNTgxODY5ODI@._V1_.jpg',
     tittle : 'Most Eligible Bachelor',
     aurthor : 'Akhil Akkineni, Pooja Hegde ',
-    price : '₹699',
+    price : 'Rating: 6.1/10',
   };
 
   return (
